0e81168ee0b91db66713895923025dda1b5f991e
hannanel100 <hannanel100@gmail.com>
2022-02-21 14:31:08 +0200

    bootstraped npm-workspace

---
+++ web/modules/home/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import { Header } from "@fstr/components";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <Header text='from home' />
+    </div>
+  );
+}
+
+export default App;
